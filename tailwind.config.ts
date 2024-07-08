dc96586446c88e750fb7902a04c41dab79ca9ee5
Ali Vayani <ila.00123ivay@gmail.com>
2024-07-08 15:04:19 -0500

    scrolling animation made

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,14 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        'text': '#f1eee9',
+        'background': '#110c05',
+        'primary': '#e4dbd1',
+        'secondary': '#546640',
+        'accent': '#7aa56f'
+
+      }
     },
   },
   plugins: [],
