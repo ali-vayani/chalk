4a4758c411d10abcf9629fde2c560a6a1db4f8c4
Ali Vayani <ila.00123ivay@gmail.com>
2024-07-10 16:36:18 -0500

    i should really grind this out

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,10 +8,11 @@ export default function Home() {
       <div className="w-40 h-full fixed test"/>
       <DashedLine/>
 
-      <div className="flex flex-col p-6">
+      <div className="flex flex-col p-6 h-screen">
         <h1 className="text-text text-3xl fixed">chalk</h1>
-        <div className="w-full h-full">
-          
+        <div className="w-full h-full flex flex-col justify-center pl-48">
+          <h1 className="text-text text-8xl mb-6">Write Your Future With <br/><span className="text-accent">Chalk</span> Tutoring</h1>
+          <button className="px-5 py-2 bg-accent text-text rounded-lg text-xl w-40">Learn More</button>
         </div>
       </div>
 
