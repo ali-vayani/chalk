18534035455f1c37df49d3e8d37ddce2e48d6f8b
Ali Vayani <ila.00123ivay@gmail.com>
2024-07-15 23:26:28 -0500

    meet the team

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import DashedLine from "./components/dashedLine";
+import MeetCard from "./components/meetCard";
 
 export default function Home() {
   return (
@@ -14,6 +15,7 @@ export default function Home() {
           <button className="px-5 py-2 bg-accent text-text rounded-lg text-xl w-40">Learn More</button>
         </div>
       </div>
+
       <div className="flex flex-col h-screen">
         <div className="w-full h-full flex flex-col justify-center pr-36 items-end">
           <h1 className="text-text text-7xl mb-12"><span className="text-accent">Who</span> are we?</h1>
@@ -21,6 +23,23 @@ export default function Home() {
             Adipiscing Elit, Sed Do <span className="text-accent">Eiusmod</span>  Tempor <br />
             <span className="text-accent">Incididunt</span> Ut Labore Et Dolore Magna
           </p>
+        </div>
+      </div>
+      
+      <div className="flex flex-col h-screen">
+        <div className="w-full h-full flex flex-col justify-start pl-48 mt-48">
+          <h1 className="text-text text-7xl mb-24"><span className="text-accent">Meet</span> the team!</h1>
+          <div className="w-full flex justify-center items-center">
+            <div className="grid-container gap-y-24 w-5/6">
+              <MeetCard blurb={"Lorem  ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod  tempor incididunt ut labore et dolore magna aliqua. Pharetra vel turpis  nunc eget lorem..."} imgPath={null}/>
+              <MeetCard blurb={"Lorem  ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod  tempor incididunt ut labore et dolore magna aliqua. Pharetra vel turpis  nunc eget lorem..."} imgPath={null}/>
+              <MeetCard blurb={"Lorem  ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod  tempor incididunt ut labore et dolore magna aliqua. Pharetra vel turpis  nunc eget lorem..."} imgPath={null}/>
+              <MeetCard blurb={"Lorem  ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod  tempor incididunt ut labore et dolore magna aliqua. Pharetra vel turpis  nunc eget lorem..."} imgPath={null}/>
+              <MeetCard blurb={"Lorem  ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod  tempor incididunt ut labore et dolore magna aliqua. Pharetra vel turpis  nunc eget lorem..."} imgPath={null}/>
+              <MeetCard blurb={"Lorem  ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod  tempor incididunt ut labore et dolore magna aliqua. Pharetra vel turpis  nunc eget lorem..."} imgPath={null}/>
+            </div>
+          </div>
+
         </div>
       </div>
     </div>
